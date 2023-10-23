8c9e3a948529d4a53ee488ea8075974f3bd3ec5e
Paruj Labpattanapaisan <mammothz@Parujs-MacBook-Pro.local>
2023-10-24 01:57:14 +0700

    feat:separate main and auth routes

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <p className="text-3xl text-blue-600">clone</p>
+      <p className="text-3xl text-blue-600">This is protected route.</p>
       <Button variant="default">Click me</Button>
     </div>
   );
